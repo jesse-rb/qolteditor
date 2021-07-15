e36c445e415b3d3e515cc4abc656c9793a1fe02f
Jesse <jesse@DESKTOP-C3UAF90.localdomain>
2021-07-15 18:03:49 +1200

    Ignore windows key and capslock key when typing

--- static/js/page.js
+++ static/js/page.js
@@ -32,7 +32,8 @@ function page(parentElement, width, height, indentTop, indentRight, indentBottom
             _caretManager.SyncedMove(-1);
         } else if (e.key == 'Backspace') {
             _caretManager.SyncedBackspace();
-        } else if (!e.meta && !e.ctrlKey && !e.altKey && !(e.key == 'Shift')) { // All general keys
+        } else if (!e.metaKey && !e.ctrlKey && !e.altKey && !(e.key == 'CapsLock') && !(e.key == 'Shift')) { // All general keys
+            console.log('General key: ', e);
             if (e.key == 'Enter') {
                 _caretManager.SyncedInsert('\n');
             } else {
@@ -43,13 +44,20 @@ function page(parentElement, width, height, indentTop, indentRight, indentBottom
         e.preventDefault();
     }
 
+    let paste = function(e) {
+        e.preventDefault();
+    }
+
     this.GetSelection = function() {
         return _selected;
     }
 
     this.Render = function() {
         _element = document.createElement('div');
+
         _element.addEventListener('keydown', pageInput, false);
+        _element.addEventListener('contextmenu', paste, false);
+
         _element.contentEditable = true;
 
         _element.style.setProperty('width', _width+'px');
@@ -65,6 +73,6 @@ function page(parentElement, width, height, indentTop, indentRight, indentBottom
         parentElement.appendChild(_element);
 
         _caretManager = new caretManager(_element);
-        _caretManager.Add(0, _cursor, '#AA00AA'); // Add default caret
+        _caretManager.Add(0, _cursor, '#FFA500'); // Add default caret
     }
 }
